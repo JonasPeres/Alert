a6ae0035724cf9d6edbd16fe162cff44a987758e
Jonas Peres <jonasperes10@hotmail.com>
2022-05-04 10:33:47 -0300

    Initial Dashboard

---
+++ src/pages/dashboard/components/Carousel.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="col-12 row q-pa-md">
+    <q-carousel
+      style="width: 100%; height: 100%; border-radius: 8px;"
+      animated
+      v-model="slide"
+      navigation
+      infinite
+      :autoplay="autoplay"
+      arrows
+      transition-prev="slide-right"
+      transition-next="slide-left"
+      @mouseenter="autoplay = false"
+      @mouseleave="autoplay = true"
+    >
+      <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
+      <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
+      <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
+      <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
+    </q-carousel>
+  </div>
+</template>
+
+<script lang="ts">
+import AbstractComponent from 'src/components/abstract-component'
+
+export default class Carousel extends AbstractComponent {
+
+  slide = 1
+  autoplay = true
+}
+</script>
